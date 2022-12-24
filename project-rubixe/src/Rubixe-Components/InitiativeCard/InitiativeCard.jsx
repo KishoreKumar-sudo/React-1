9f26bab3a2feb111a7062974fc3be199dfd043e4
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2022-12-24 13:35:31 +0530

    okay

--- project-rubixe/src/Rubixe-Components/InitiativeCard/InitiativeCard.jsx
+++ project-rubixe/src/Rubixe-Components/InitiativeCard/InitiativeCard.jsx
@@ -14,7 +14,7 @@ const InitiativeCard = () => {
         <div className='card-1'>
 
           <div className="card-head radius-top" style={{ backgroundColor: '#1d2d60' }}>
-            <div className="card-circle up"><h3>01</h3></div>
+            <div className="card-circle up"><h3 style={{color:'#1d2d60'}}>01</h3></div>
           </div>
           <div className="card-body">
             <img src={cardImg1} alt="" />
@@ -29,10 +29,10 @@ const InitiativeCard = () => {
             <div className='card-text'> Gain awareness on Al and build an interactive story around it</div>
             <img src={cardImg2} alt="" />
           </div>
-          <div className="card-head radius-bottom card-head-down" style={{ backgroundColor: '#9d2057' }}><div className="card-circle down"><h3>02</h3></div></div>
+          <div className="card-head radius-bottom card-head-down" style={{ backgroundColor: '#9d2057' }}><div className="card-circle down"><h3 style={{color:'#9d2057'}}>02</h3></div></div>
         </div>
         <div className='card-1'>
-          <div className="card-head radius-top" style={{ backgroundColor: '#e35e13' }}><div className="card-circle up"><h3>03</h3></div></div>
+          <div className="card-head radius-top" style={{ backgroundColor: '#e35e13' }}><div className="card-circle up"><h3 style={{color:'#e35e13'}}>03</h3></div></div>
           <div className="card-body">
             <img src={cardImg3} alt="" />
             <div className='card-text'> Acquire programming skills in a user-friendly format</div>
@@ -45,10 +45,10 @@ const InitiativeCard = () => {
             <div className='card-text'> Exposure to mini projects on diverse topics</div>
             <img src={cardImg4} alt="" />
           </div>          
-          <div className="card-head radius-bottom card-head-down" style={{ backgroundColor: '#195b37' }}><div className="card-circle down"><h3>04</h3></div></div>
+          <div className="card-head radius-bottom card-head-down" style={{ backgroundColor: '#195b37' }}><div className="card-circle down"><h3 style={{color:'#195b37'}}>04</h3></div></div>
         </div>
         <div className='card-1'>
-          <div className="card-head radius-top" style={{ backgroundColor: '#b77d33' }}><div className="card-circle up"><h3>05</h3></div></div>
+          <div className="card-head radius-top" style={{ backgroundColor: '#b77d33' }}><div className="card-circle up"><h3 style={{color:'#b77d33'}} >05</h3></div></div>
           <div className="card-body">
             <img src={cardImg5} alt="" />
             <div className='card-text'> Train the teachers programme</div>
