c45072f55fb609ed603b41553ec5102c7ef09e13
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-01-03 23:04:44 +0530

    validation

--- form-validation/src/Registration_Form/RegistrationForm.jsx
+++ form-validation/src/Registration_Form/RegistrationForm.jsx
@@ -5,47 +5,88 @@ const RegistrationForm = () => {
     name: "",
     email: "",
     mobilenumber: "",
-    passsword: "",
-    reenter: ""
+    password: "",
+    re_enter: ""
   })
   const [nameerr, setNameErr] = useState()
   const [emailerr, setEmailErr] = useState()
   const [mobilenumbererr, setmobileNumberErr] = useState()
   const [passworderr, setPasswordErr] = useState()
-  const [reentererr, setReenterErr] = useState()
+  const [reEnterErr, setReEnterErr] = useState()
+
+  let getData = (event) => {
+    setvalues({ ...values, [event.target.name]: event.target.value })
+  }
+
+  let validateForm = (event) => {
+    event.preventDefault()
+    let name = values.name
+    let email = values.email
+    let mobilenumber = values.mobilenumber
+    let password = values.password
+    let re_enter = values.re_enter
+
+    //validation for name
+    if (name === "") {
+      setNameErr("Plz enter ur name")
+    }
+    else if (name.length < 4 || name.length > 15) {
+      setNameErr("Name must contain min 4 & max 15 characters")
+    }
+    else{
+      setNameErr("")
+    }
+
+    //validation for email
+    if (email === ""){
+      setEmailErr("Plz enter ur email")
+    }
+    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setEmailErr("Plz enter valid Email")
+    }
+    else{
+      setEmailErr("")
+    }
+  }
+
+  //(?=.*?[A-Z]) for uppercase
+  //(?=.*?[a-z]) for lower
+  //(?=.*?[0-9]) for one digit
+  //(?=.*?[#?!@$%^&*-]) for special character
+
 
   return <>
     <div className="container mt-5">
       <pre>{JSON.stringify(values)}</pre>
       <div className="row">
         <div className="col-md-6">
-          <form>
+          <form onSubmit={validateForm}>
             <div className="card">
               <div className="card-header text-success">
                 <h3>Registration-Form Validation Using React</h3>
               </div>
               <div className="card-body">
                 <div className='form-group'>
-                  <input type="text" placeholder='Name' name='name' className='form-control' onChange={values} />
-                  <span className='text-danger' >Enter your valid Name</span>
+                  <input type="text" placeholder='Name' name='name' className='form-control' onChange={getData} />
+                  <span className='text-danger'>{nameerr}</span>
                 </div>
                 <div className='form-group'>
-                  <input type="text" placeholder='Email' name='email' className='form-control' onChange={values} />
-                  <span className='text-danger' >Enter valid Email-Id</span>
+                  <input type="text" placeholder='Email' name='email' className='form-control' onChange={getData} />
+                  <span className='text-danger'>{emailerr}</span>
                 </div>
                 <div className='form-group'>
-                  <input type="number" placeholder='Mobile Number' name='mobilenumber' className='form-control' onChange={values} />
-                  <span className='text-danger' >Enter valid Mobile-Number</span>
+                  <input type="number" placeholder='Mobile Number' name='mobilenumber' className='form-control' onChange={getData} />
+                  <span className='text-danger'></span>
                 </div>
                 <div className='form-group'>
-                  <input type="password" placeholder='Password' name='password' className='form-control' onChange={values} />
-                  <span className='text-danger' >Enter your correct password</span>
+                  <input type="password" placeholder='Password' name='password' className='form-control' onChange={getData} />
+                  <span className='text-danger'></span>
                 </div>
                 <div className='form-group'>
-                  <input type="password" placeholder='Re-Enter Password' name='reenter' className='form-control' onChange={values} />
-                  <span className='text-danger' >Re-enter your password</span>
+                  <input type="password" placeholder='Re-Enter Password' name='re_enter' className='form-control' onChange={getData} />
+                  <span className='text-danger'></span>
                 </div>
-                <input type="button" value="Submit" className='btn btn-warning' />
+                <input type="submit" value="Submit" className='btn btn-warning' />
               </div>
             </div>
           </form>
