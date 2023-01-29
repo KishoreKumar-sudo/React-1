4e5f25e51a0342a2d325ca2c1584d53322a82435
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-01-29 23:25:55 +0530

    axios

--- Completed Apps/Product-CRUD Local Server/src/Components/Products.jsx
+++ Completed Apps/Product-CRUD Local Server/src/Components/Products.jsx
@@ -14,45 +14,45 @@ const Products = () => {
     Axios.get("http://localhost:3000/products")
       .then((response) => {
         setProducts(response.data)
-       setLoading(false)
+        setLoading(false)
       })
-      .catch(() => { setLoading(true)})
+      .catch(() => { setLoading(true) })
   }, [])
   return (
     <>
       <div className="container">
         <pre>{JSON.stringify(products)}</pre>
         <div className="row">
-          
-            {
-              products.length > 0 ? <>
-          
-                  { 
-                    products.map((product) => {
-                      return <div className="col-md-3">
-                        <div className="card mt-5">
-                          <div className="card-header">
-                            <center> <img id="img" src={product.image} width="150px" height="150px" alt="No pic" /></center>
-                          </div>
-                          <div className="card-body">
-                            <ul className="list-group">
-                              <li className="list-group-item" >Name:{product.name}</li>
-                              <li className="list-group-item" >Price:{product.price}</li>
-                              <li className="list-group-item" >Qty:{product.qty}</li>
-                              <li className="list-group-item" >Total:{product.qty * product.price}</li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
 
-                    })
-                  }
-                
+          {
+            products.length > 0 ? <>
+
+              {
+                products.map((product) => {
+                  return <div className="col-md-3">
+                    <div className="card mt-5">
+                      <div className="card-header">
+                        <center> <img id="img" src={product.image} width="150px" height="150px" alt="No pic" /></center>
+                      </div>
+                      <div className="card-body">
+                        <ul className="list-group">
+                          <li className="list-group-item" >Name:{product.name}</li>
+                          <li className="list-group-item" >Price:{product.price}</li>
+                          <li className="list-group-item" >Qty:{product.qty}</li>
+                          <li className="list-group-item" >Total:{product.qty * product.price}</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                })
+              }
+
 
             </> : <>*** No Products ***</>
           }
-            
-          
+
+
         </div>
       </div>
 
